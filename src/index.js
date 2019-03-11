c0593183679660edfcc2838d3999d4c6f4dac3e1
Raman Nerad <romaus@mail.ru>
2019-03-11 20:01:33 +0300

    feat: add function that checks string according to brackets config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let testStr = [];
+  for (i = 0; i < str.length; i++) {
+    testStr.push(str[i])
+  }
+  for (i = 0; i < bracketsConfig.length; i++) {
+    let count = testStr.length;
+    for (j = 0; j < testStr.length; j++) {
+      if (testStr[j] == bracketsConfig[i][0] && testStr[j+1] == bracketsConfig[i][1]) {
+        testStr.splice(j,2);
+        j = -1;
+      }
+    }
+    if (testStr.length < count) {
+      i = -1;
+    }
+  }
+  return !testStr.length;
 }
